1edb0ece949fd80f34310d29f8f854983d0f91d4
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 12:38:07 +0100

    Robust API

--- Assets/JS/CurseForgeAPI.js
+++ Assets/JS/CurseForgeAPI.js
@@ -5,7 +5,7 @@ function getCurseForgeDescription(asset) {
             let json = JSON.parse(result);
             console.log(json);
             document.getElementById('icon').src = json.data.logo.url;
-            document.getElementById('versions').innerHTML = json.data.latestFiles[0].gameVersions[0]+' - '+json.data.latestFiles[json.data.latestFiles.length - 1].gameVersions[0];
+            document.getElementById('versions').innerHTML = json.data.latestFilesIndexes[0].gameVersion+' - '+json.data.latestFilesIndexes[json.data.latestFilesIndexes.length - 1].gameVersion;
             document.getElementById('body').innerHTML = markdownToHTML(json.body);
         })
         .catch((error) => console.error(error));
