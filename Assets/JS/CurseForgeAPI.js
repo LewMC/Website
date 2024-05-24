4c2f4d12892b71da18b22e478d9c4e6a1dc64eec
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 12:45:26 +0100

    Testing

--- Assets/JS/CurseForgeAPI.js
+++ Assets/JS/CurseForgeAPI.js
@@ -3,7 +3,6 @@ function getCurseForgeOverview(asset) {
         .then((response) => response.text())
         .then((result) => {
             let json = JSON.parse(result);
-            console.log(json);
             document.getElementById('icon').src = json.data.logo.url;
             document.getElementById('versions').innerHTML = json.data.latestFilesIndexes[0].gameVersion+' - '+json.data.latestFilesIndexes[json.data.latestFilesIndexes.length - 1].gameVersion;
         })
@@ -15,7 +14,6 @@ function getCurseForgeDescription(asset) {
         .then((response) => response.text())
         .then((result) => {
             let json = JSON.parse(result);
-            console.log(json);
             document.getElementById('body').innerHTML = markdownToHTML(json.data);
         })
         .catch((error) => console.error(error));
@@ -27,10 +25,11 @@ function getCurseForgeVersions(asset) {
             .then((response) => response.text())
             .then((result) => {
                 let json = JSON.parse(result);
+                console.log(json);
                 fetchedVersions = true;
                 let versionsGrid = '';
                 let foundFirstRelease = false;
-                for (let version in json) {
+                for (let version in json.data) {
                     if (json[version].data.fileStatus === 1) {
                         versionsGrid += `<a target="_blank" href="https://curseforge.com/minecraft/modpack/${asset}/files/${json[version].data.id}" class="card-link relative" id="${json[version].data.id}"><h3>${json[version].data.displayName}</h3><p>For Minecraft ${json[version].data.gameVersions[0]} - ${json[version].data.gameVersions[json[version].data.gameVersions.length - 1]}</p></a>`;
                     }
