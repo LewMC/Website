37f89a046a653088eeaeb80f4ba5034bc526f010
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 21:05:44 +0100

    Replace old meta stuff.

--- Views/Common/Head.php
+++ Views/Common/Head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Welcome to LewMC. The home of Galactica, PackStats, and so much more.">
+<meta name="description" content="Welcome to LewMC. The home of Galactica, Essence, and so much more.">
 <meta name="author" content="LewMC">
-<meta name="keywords" content="Lew's Jurassic Pack, Galactica, PackStats, SimplyAesthetic, LewMC">
+<meta name="keywords" content="Lew's Jurassic Pack, Galactica, Essence, Kryptonite, SimplyAesthetic, LewMC">
 <link rel="stylesheet" href="/Assets/CSS/Dist.css">
