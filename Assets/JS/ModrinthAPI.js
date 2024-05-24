2bb1ae08813010b7a141609d23f6b3395ed27f0d
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 10:53:51 +0100

    Fix bug.

--- Assets/JS/ModrinthAPI.js
+++ Assets/JS/ModrinthAPI.js
@@ -20,8 +20,8 @@ function getModrinthVersions(asset) {
                 let json = JSON.parse(result);
                 fetchedVersions = true;
                 let versionsGrid = '';
+                let foundFirstRelease = false;
                 for (let version in json) {
-                    let foundFirstRelease = false;
                     if (json[version].status === "listed") {
                         if (json[version].version_type === "beta") {
                             versionsGrid += `<a target="_blank" href="https://modrinth.com/plugin/${asset}/version/${json[version].version_number}" class="card-link relative" id="${json[version].version_number}"><h3>${json[version].name}</h3><p>For Minecraft ${json[version].game_versions[0]} - ${json[version].game_versions[json[version].game_versions.length - 1]}</p><span class="absolute top-0 right-0 bg-yellow-500 rounded-bl-md px-1 text-black">SNAPSHOT</span></a>`;
