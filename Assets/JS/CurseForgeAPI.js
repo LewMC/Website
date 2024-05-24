e46318edb40ae59b569df858758c2353db7f81a3
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 12:22:16 +0100

    Add CurseForge logo.

--- Assets/JS/CurseForgeAPI.js
+++ Assets/JS/CurseForgeAPI.js
@@ -5,7 +5,7 @@ function getCurseForgeDescription(asset) {
         .then((response) => response.text())
         .then((result) => {
             let json = JSON.parse(result);
-            document.getElementById('icon').src = json.icon_url;
+            document.getElementById('icon').src = json.data.logo.url;
             document.getElementById('versions').innerHTML = json.game_versions[0]+' - '+json.game_versions[json.game_versions.length - 1];
             document.getElementById('body').innerHTML = markdownToHTML(json.body);
         })
