bc1c8400d93cb38c4b6ae774dcc2b743a996a4d5
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 09:51:51 +0100

    Add versions

--- Assets/JS/Asset.js
+++ Assets/JS/Asset.js
@@ -6,6 +6,10 @@ function getModrinthDescription(asset) {
 
     fetch('/api/modrinth?description='+ asset)
         .then((response) => response.text())
-        .then((result) => console.log(result))
+        .then((result) => {
+            console.log(result)
+            let json = JSON.parse(result);
+            document.getElementById('versions').innerHTML = json.game_versions[0]+' - '+json.game_versions[json.game_versions.length - 1];
+        })
         .catch((error) => console.error(error));
 }
