8fdc0a611d0440928b164a79776017585e6e422f
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 12:44:19 +0100

    Add version API

--- API/CurseForge.php
+++ API/CurseForge.php
@@ -44,4 +44,25 @@ if (isset($_GET['overview'])) {
 
     curl_close($curl);
     echo $response;
+} elseif (isset($_GET['versions'])) {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.curseforge.com/v1/mods/'.htmlspecialchars($_GET['description']).'/files',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'x-api-key: '.CURSEFORGE_API_KEY
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
 }
--- Assets/JS/CurseForgeAPI.js
+++ Assets/JS/CurseForgeAPI.js
@@ -20,3 +20,23 @@ function getCurseForgeDescription(asset) {
         })
         .catch((error) => console.error(error));
 }
+
+function getCurseForgeVersions(asset) {
+    if (fetchedVersions === false) {
+        fetch('/api/curseforge?versions='+ asset)
+            .then((response) => response.text())
+            .then((result) => {
+                let json = JSON.parse(result);
+                fetchedVersions = true;
+                let versionsGrid = '';
+                let foundFirstRelease = false;
+                for (let version in json) {
+                    if (json[version].data.fileStatus === 1) {
+                        versionsGrid += `<a target="_blank" href="https://curseforge.com/minecraft/modpack/${asset}/files/${json[version].data.id}" class="card-link relative" id="${json[version].data.id}"><h3>${json[version].data.displayName}</h3><p>For Minecraft ${json[version].data.gameVersions[0]} - ${json[version].data.gameVersions[json[version].data.gameVersions.length - 1]}</p></a>`;
+                    }
+                }
+                document.getElementById('download').innerHTML = versionsGrid;
+            })
+            .catch((error) => console.error(error));
+    }
+}
