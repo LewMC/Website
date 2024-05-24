e57a4a030f55109c9c4e6fb925be899cda535375
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 09:16:50 +0100

    Add router.

--- index.php
+++ index.php
@@ -6,5 +6,9 @@ require_once __DIR__ . '/starlight/HTTP/Router.php';
 
 $Router = new Router();
 
+// Plugins
 $Router->route('/plugin/essence', '/Views/Asset.php');
 $Router->route('/plugin/kryptonite', '/Views/Asset.php');
+
+// API
+$Router->route('/api/hangar', '/API/Hangar.php');
