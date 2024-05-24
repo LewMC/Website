59769b4007ce6516b29f7cf42c4f4e6169e2120a
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 10:02:57 +0100

    Fix bug.

--- Assets/JS/Asset.js
+++ Assets/JS/Asset.js
@@ -6,7 +6,7 @@ function getModrinthDescription(asset) {
             let json = JSON.parse(result);
             document.getElementById('icon').src = json.icon_url;
             document.getElementById('versions').innerHTML = json.game_versions[0]+' - '+json.game_versions[json.game_versions.length - 1];
-            document.getElementById('body').innerHTML = json.body.replace(new RegExp('\r?\n','g'), '<br>');
+            json.body = json.body.replace(new RegExp('\r?\n','g'), '<br>');
             document.getElementById('body').innerHTML = json.body.replace(new RegExp('\\*','g'), '-');
         })
         .catch((error) => console.error(error));
