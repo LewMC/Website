3297211ace874b1940403b5920ae29776555a74a
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 11:33:21 +0100

    Cors is annoying.

--- Assets/JS/LewMCAPI.js
+++
@@ -1,9 +0,0 @@
-function getDownloadCount(asset) {
-    fetch('https://service.lewmc.net/download-counter?resource='+ asset)
-        .then((response) => response.text())
-        .then((result) => {
-            console.log(result)
-            document.getElementById(asset+'-downloads').innerHTML = JSON.parse(result).data.total;
-        })
-        .catch((error) => console.error(error));
-}
