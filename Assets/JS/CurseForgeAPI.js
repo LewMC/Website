09abcd38e72d48aded63683a53b39680f8ba4348
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2024-05-24 12:31:10 +0100

    Testing.

--- Assets/JS/CurseForgeAPI.js
+++ Assets/JS/CurseForgeAPI.js
@@ -2,6 +2,7 @@ function getCurseForgeDescription(asset) {
     fetch('/api/curseforge?description='+ asset)
         .then((response) => response.text())
         .then((result) => {
+            console.log(result);
             let json = JSON.parse(result);
             document.getElementById('icon').src = json.data.logo.url;
             document.getElementById('versions').innerHTML = json.game_versions[0]+' - '+json.game_versions[json.game_versions.length - 1];
